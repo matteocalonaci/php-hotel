e23ca1b8ca755cf49138b8d4618afa23fe77d202
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-11 15:41:21 +0200

    Stampo in pagina la tabella di bootstrap con le informazioni dell'array ciclate tramite  for each.

--- index.php
+++ index.php
@@ -48,11 +48,84 @@ $hotels = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
     <link rel="stylesheet" href="./CSS/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>php-hotel</h1>
-  
+    <h1 class="d-flex justify-content-center p-2">php-hotel</h1>
+    <hr>
+
+    <!-- stampo l'array hotels in pagina -->
+    <!-- <pre>
+         <?= var_dump($hotels); ?>  
+    </pre> -->
+
+    <!-- <h3>Stampo elemento singolo</h3>
+    <p><?= $hotels[0]["name"] ?></p> -->
+
+
+    <!-- stampo con ciclo for each -->
+    <!-- <h3>Ciclo ForEach</h3>
+    <?php
+    foreach($hotels as $hotel) {
+        echo "<p>" . $hotel["name"] . " " .  $hotel["vote"]. " " .  $hotel["distance_to_center"] . "</p>";
+    }
+    ?> -->
+
+<table class="table mt-5">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">hotel 1</th>
+      <th scope="col">hotel 2</th>
+      <th scope="col">hotel 3</th>
+      <th scope="col">hotel 4</th>
+      <th scope="col">hotel 5</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">name</th>
+      <?php
+    foreach($hotels as $hotel) {
+        echo "<td>" . $hotel["name"] . "</td>";
+    }
+    ?>
+    </tr>
+    <tr>
+      <th scope="row">description</th>
+      <?php
+    foreach($hotels as $hotel) {
+        echo "<td>" . $hotel["description"] . "</td>";
+    }
+    ?>
+    </tr>
+    <tr>
+      <th scope="row">parking</th>
+      <?php
+    foreach($hotels as $hotel) {
+        echo "<td>" . $hotel["parking"] . "</td>";
+    }
+    ?>
+    </tr>
+    <tr>
+      <th scope="row">vote</th>
+      <?php
+    foreach($hotels as $hotel) {
+        echo "<td>" . $hotel["vote"] . "</td>";
+    }
+    ?>
+    </tr>
+    <tr>
+      <th scope="row">distance to center</th>
+      <?php
+    foreach($hotels as $hotel) {
+        echo "<td>" . $hotel["distance_to_center"] . "</td>";
+    }
+    ?>
+    </tr>
+  </tbody>
+</table>
+
+
 </body> 
 </html>
